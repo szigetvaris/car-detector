90b51f13a4b3b936f089abb5d11fe4a9439c3518
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-04-13 15:17:01 +0200

    health endpoint

--- carDetector/carDetector/views.py
+++ carDetector/carDetector/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
+def health(request):
+    return HttpResponse("OK")
+
 def index(request):
     return HttpResponse("Hello, world. You're at the carDetector index.")
