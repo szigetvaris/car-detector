2bafa1dc06d222c6226a0a7a6db5d2c5767ac827
szigetvaris <115147728+szigetvaris@users.noreply.github.com>
2024-04-14 11:21:56 +0200

    Delete carDetector directory
    using flask server instead

--- carDetector/carDetector/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def health(request):
-    return HttpResponse("OK")
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the carDetector index.")
