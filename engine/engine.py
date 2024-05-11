e9e6d831b570a740c8f7f6d1c4f181b244bf723f
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-05-11 12:45:57 +0200

    engine modifications

--- engine/engine.py
+++ engine/engine.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, send_file
 from recognition import car_detector
 import pika
 import csv
+import cv2
 
 engine = Flask(__name__)
 
@@ -10,6 +11,31 @@ engine = Flask(__name__)
 @engine.route("/health")
 def health():
     return "OK"
+
+def car_detector(file_path):
+    # get actual path
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(current_path, file_path)
+    # Load the image
+    image = cv2.imread(file_path)
+    
+    # Load the car detector model
+    xml_path = os.path.join(current_path, 'haarcascade_car.xml')
+    car_detector = cv2.CascadeClassifier(xml_path)
+
+    # Detect cars in the image
+    cars = car_detector.detectMultiScale(image)
+
+    # Draw bounding boxes around the cars
+    for (x, y, w, h) in cars:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    # Save the image with the bounding boxes
+    output_path = file_path.split('.')[0] + '_detected.jpg'
+    cv2.imwrite(output_path, image)
+
+    # Return the number of cars detected and the new file path
+    return len(cars), output_path
 
 
 @engine.route("/detect", methods=["POST"])
--- engine/recognition.py
+++ engine/recognition.py
@@ -14,29 +14,29 @@
 import cv2 # opencv-python
 import os
 
-def car_detector(file_path):
-    # get actual path
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(current_path, file_path)
-    # Load the image
-    image = cv2.imread(file_path)
+# def car_detector(file_path):
+#     # get actual path
+#     current_path = os.path.dirname(os.path.realpath(__file__))
+#     file_path = os.path.join(current_path, file_path)
+#     # Load the image
+#     image = cv2.imread(file_path)
     
-    # Load the car detector model
-    xml_path = os.path.join(current_path, 'haarcascade_car.xml')
-    car_detector = cv2.CascadeClassifier(xml_path)
+#     # Load the car detector model
+#     xml_path = os.path.join(current_path, 'haarcascade_car.xml')
+#     car_detector = cv2.CascadeClassifier(xml_path)
 
-    # Detect cars in the image
-    cars = car_detector.detectMultiScale(image)
+#     # Detect cars in the image
+#     cars = car_detector.detectMultiScale(image)
 
-    # Draw bounding boxes around the cars
-    for (x, y, w, h) in cars:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+#     # Draw bounding boxes around the cars
+#     for (x, y, w, h) in cars:
+#         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-    # Save the image with the bounding boxes
-    output_path = file_path.split('.')[0] + '_detected.jpg'
-    cv2.imwrite(output_path, image)
+#     # Save the image with the bounding boxes
+#     output_path = file_path.split('.')[0] + '_detected.jpg'
+#     cv2.imwrite(output_path, image)
 
-    # Return the number of cars detected and the new file path
-    return len(cars), output_path
+#     # Return the number of cars detected and the new file path
+#     return len(cars), output_path
 
 car_detector('tmp/cars.jpg')
