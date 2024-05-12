876d81730a3e3bf6df03290f8d9d5727f2894be2
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-05-12 07:45:00 +0200

    app modifications

--- carDetector/app.py
+++ carDetector/app.py
@@ -81,4 +81,4 @@ def home():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, allow_unsafe_werkzeug=True)
