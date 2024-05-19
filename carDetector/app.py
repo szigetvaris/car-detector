e5494f9fe593bbd5bdbdf226cd694be3105a5a44
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-05-19 19:17:47 +0200

    set werkzeug true

--- carDetector/app.py
+++ carDetector/app.py
@@ -108,4 +108,4 @@ def home():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
