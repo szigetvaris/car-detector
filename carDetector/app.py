4a0ffccb40a25f252e95087503f91e17912e01e0
Szigetvari Simon <szigetvari.simon@gmail.com>
2024-05-19 18:39:24 +0200

    app rabbit connect changed

--- carDetector/app.py
+++ carDetector/app.py
@@ -4,6 +4,7 @@ import base64
 from flask_socketio import SocketIO, emit
 import pika
 import threading
+import time
 
 app = Flask(__name__)
 
@@ -33,51 +34,55 @@ def upload():
     else:
         return 'No data received'
 
-
+# Original solution, AMQP exception
 # Initialize Flask-SocketIO
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-# RabbitMQ setup
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='my_queue')
-
-def callback(ch, method, properties, body):
-    # When a message is received, emit it to the WebSocket
-    socketio.emit('message', {'data': body.decode()})
-
-# Start consuming messages from RabbitMQ in a new thread
-def start_consuming():
-    channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
-    channel.start_consuming()
-
-threading.Thread(target=start_consuming).start()
-
-
-# Initalize Flask-SocketIO
 # socketio = SocketIO(app, cors_allowed_origins="*")
 
+# # RabbitMQ setup
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# channel = connection.channel()
+# channel.queue_declare(queue='my_queue')
+
 # def callback(ch, method, properties, body):
-#     # Wjem a ,essage os receoved, emit it to the WebSocket
+#     # When a message is received, emit it to the WebSocket
 #     socketio.emit('message', {'data': body.decode()})
 
 # # Start consuming messages from RabbitMQ in a new thread
 # def start_consuming():
-#     try:
-#         connection = pika.BlockingConnection(pika.ConnectionParameters('0.0.0.0'))
-#         channel = connection.channel()
-#         channel.queue_declare(queue='car_detector')
+#     channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
+#     channel.start_consuming()
 
-#         def callback(ch, method, properties, body):
-#             # When a message is received, emit it to the WebSocket
-#             socketio.emit('message', {'data': body.decode()})
+# threading.Thread(target=start_consuming).start()
 
-#         channel.basic_consume(queue='car_detector', on_message_callback=callback, auto_ack=True)
-#         channel.start_consuming()
-#     except Exception as e:
-#         print(f"Error in start_consuming: {e}")
+# retry till rabbitMQ is stable
+# Initalize Flask-SocketIO
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-# # threading.Thread(target=start_consuming).start()
+def callback(ch, method, properties, body):
+    # Wjem a ,essage os receoved, emit it to the WebSocket
+    socketio.emit('message', {'data': body.decode()})
+
+# Start consuming messages from RabbitMQ in a new thread
+def start_consuming():
+    while True:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+            channel = connection.channel()
+            channel.queue_declare(queue='car_detector')
+
+            def callback(ch, method, properties, body):
+                # When a message is received, emit it to the WebSocket
+                socketio.emit('message', {'data': body.decode()})
+
+            channel.basic_consume(queue='car_detector', on_message_callback=callback, auto_ack=True)
+            channel.start_consuming()
+            # If we've made it here, we've successfully connected and can break the loop
+            break
+        except pika.exceptions.AMQPConnectionError as e:
+            print("Failed to connect to RabbitMQ. Retrying...")
+            time.sleep(5)
+
+threading.Thread(target=start_consuming).start()
 
 @app.route('/admin')
 def admin():
